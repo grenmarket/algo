d27609c6102fa3be6e697505609704a26f5befa8
Slawek Blicharz <sbandq@gmail.com>
2025-02-17 17:30:56 +0100

    Knuth-Morris-Pratt recursive prefix-suffix computation

--- KMP.py
+++ KMP.py
@@ -1,3 +1,6 @@
+from typing import List
+
+
 def prefix_table(s):
     pref = [0]
     k = 0
@@ -21,3 +24,18 @@ def kmp(text, pattern):
         if k == len(pattern):
             print(str(n - k))
             k = pt[k-1]
+
+
+
+def pref_table_recursive(s):
+    return _pref_table_recursive(s, len(s) - 1)
+
+def _pref_table_recursive(s, i):
+    if i == 0:
+        return [0]
+    prev = _pref_table_recursive(s, i - 1)
+    if s[i] == s[prev[-1]]:
+        prev.append(prev[-1] + 1)
+    else:
+        prev.append(0 if s[i] != s[0] else 1)
+    return prev
